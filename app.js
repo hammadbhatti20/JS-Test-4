c86e2dbbdc9198d580aa6936e98702c6fa25b654
hammadbhatti20 <hammadaslam1052000@gamil.com>
2024-03-04 11:26:25 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//Chapter No: 14-16
+
+//Q1
+// var studentNames = [];
+// studentNames.push("hammad");
+// studentNames.push("Sabtain");
+
+//Q2
+// var studentNames = {};
+// studentNames["hammad"] = null;
+// studentNames["sabtain"] = null;
+
+//Q3
