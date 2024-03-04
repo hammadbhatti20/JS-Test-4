1efa78506a1b5f5071531ee6f7de1f467478568d
hammadbhatti20 <hammadaslam1052000@gamil.com>
2024-03-04 11:44:28 +0500

    20% complete

--- app.js
+++ app.js
@@ -11,3 +11,17 @@
 // studentNames["sabtain"] = null;
 
 //Q3
+// var str = ["Pakistan","India","Australia","Afghanistan"];
+
+//Q4
+// var num = [10,20,40,50];
+
+//Q5
+// var bool = [true,false];
+
+//Q6
+// var arr = ["Pakistan","India","Australia","Afghanistan",10,20,40,50,true,false];
+
+//Q7
+// var eduQ = ["SSC","HSC","BCS","BS","BCOM","MS","M. Phil.","PhD"];
+// document.write("<h1>Qualifications:</h1>","</br>","</br>","1) ",eduQ[0],"</br>","2) ",eduQ[1],"</br>","3) ",eduQ[2],"</br>","4) ",eduQ[3],"</br>","5) ",eduQ[4],"</br>","6) ",eduQ[5],"</br>","7) ",eduQ[6],"</br>","8) ",eduQ[7],"</br>");
